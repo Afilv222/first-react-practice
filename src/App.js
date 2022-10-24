6b6f2bcfae8d9c591f3b364f6f966f855c710827
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2022-10-24 00:14:19 -0600

    Started new react project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 // component
 
-import Todo from './components/Todo';
-
 
 function App() {
   return (
     <div>
-      <h2>My Todo</h2>
-      <Todo text = "Learn react" />
-      <Todo text = "Master react" />
-      <Todo text = "Explore the real react" />
+
     </div>
 
   );
--- src/components/Backdrop.js
+++
@@ -1,5 +0,0 @@
-function Backdrop(props){
-return <div className="backdrop" onClick={props.onClickBackDrop}></div> 
-}
-
-export default Backdrop; 
