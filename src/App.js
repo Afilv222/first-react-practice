d3051f6d7e8668e9e78f5981753f6bf9967f995b
Afilv222 <avidy222@mtroyal.ca>
2022-10-24 11:09:22 -0600

    Started creating the routes for each page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 // component
-
+import { Route } from 'react-router-dom'; 
+import AllMeetupsPage from './pages/AllMeetups.js';
+import NewMeetupPage from './pages/NewMeetup.js';
+import FavoritesPage from './pages/Favorites.js';
 
 function App() {
+  // Current dom localhost:3000
+  // actual server could use the path like my-page.com......
   return (
     <div>
+      <Route path = '/'>
+        <AllMeetupsPage />
+      </Route>
 
+      {/* <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route> */}
+      
     </div>
 
   );
