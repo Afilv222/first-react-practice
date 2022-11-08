ef28876c3137461248deb2862e298e68b9ddf6fe
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2022-11-08 00:49:30 -0700

    Fixed small mistake with duplicate ID

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 // component
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import Favorites from './pages/Favorites';
+import NewMeetup from './pages/NewMeetup';
+import MainNav from './components/layout/MainNav';
 
 function App() {
   return (
     <div>
-
+        <BrowserRouter>
+        <MainNav />
+          <Routes>
+            <Route path='/' element={<AllMeetupsPage/>}/>
+            <Route path='/new-meetup' element={<NewMeetup/>}/>
+            <Route path='/favorites' element={<Favorites/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
 
   );
